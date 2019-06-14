5bf34fdc730199c9dd96981d53d037ffe156ea3a
Andrei Egorov <andreyegorov1992@gmail.com>
2019-06-13 20:07:39 -0700

    Create PATH helper func, serve static css files

--- expressServer/routes/admin.js
+++ expressServer/routes/admin.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path =  require('path')
+const rootDir = require("../util/path")
 const router = express.Router();
 
 // real path is /admin/add-product => Get
 router.get("/add-product", (req, res, next) => {
     //sending html file instead of ugly markup
-    res.sendFile(path.join(__dirname, "../", "views/", "add-product.html" ))
+    res.sendFile(path.join(rootDir, "views/", "add-product.html" ))
     // res.send("<form action='/admin/add-product' method='POST'><input type='text' name='title'> <button>Add product</button></form>")
 
 });
