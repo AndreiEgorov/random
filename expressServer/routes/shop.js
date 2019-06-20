d5168c5bd56daa38722eddba2ecd006cb08c87d2
Andrei Egorov <andreyegorov1992@gmail.com>
2019-06-19 18:55:08 -0700

    Set global variable and edit styles

--- expressServer/routes/shop.js
+++ expressServer/routes/shop.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const rootDir = require("../util/path")
+const adminData = require("./admin")
 
 const routes = express.Router();
 
 routes.get("/", (req, res, next) => {
+    console.log("shop.js", adminData.products);
     //using path node module to construct a path to a file of html path.join
     res.sendFile(path.join(rootDir, 'views/', 'shop.html'))
 });
