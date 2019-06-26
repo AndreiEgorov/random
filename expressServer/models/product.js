b6a3b3328d29488c1b98e5f00de44a70f9072446
Andrei Egorov <andreiegorov@Andreis-MacBook-Pro-2.local>
2019-06-25 18:17:19 -0700

    refactor

--- expressServer/models/product.js
+++ expressServer/models/product.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 // const products = [];
+const p = path.join(__dirname, "..",  'data', 'products.json');
 
+const getProductsFromFile = (callback) =>{
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return callback([])
+        }
+        callback(JSON.parse(fileContent))
+    });
+}
 
 module.exports = class Product {
     constructor(title){
@@ -9,30 +18,15 @@ module.exports = class Product {
     }
 
     save(){
-        const p = path.join(__dirname, "..",  'data', 'products.json');
-        fs.readFile(p,  (err, fileContent)=>{
-            let products = []
-            if (!err){
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products => {
             products.push(this)
             fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err)
+                console.log("ERR", err)
             } )
         })
     }
 //since the func does not return anything, we put call back to get value from fs.readFile once it is ready
     static fetchAll(callback) {
-        const p = path.join(__dirname, "..",  'data', 'products.json');
-        fs.readFile(p, 'utf-8', (err, fileContent) => {
-            if (err) {
-                callback([])
-            }
-             callback(JSON.parse(fileContent))
-        });
-
-
+        getProductsFromFile(callback)
     }
-
-
 }
