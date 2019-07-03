ba70fa22cc970b43b9ba065e204eba34ceffe6d6
Andrei Egorov <andreyegorov1992@gmail.com>
2019-07-02 20:34:31 -0700

    add EDIT PRODUCT functionality

--- expressServer/controllers/admin.js
+++ expressServer/controllers/admin.js
@@ -13,15 +13,12 @@ module.exports.getEditProduct = (req, res, next) => {
     if (!editMode){
         return res.redirect('/')
     }
-    console.log("HER", req.params.productId)
     const productId = req.params.productId;
     Product.findById(productId,  product => {
         if (!product){
             console.error("NO product found")
             return res.redirect("/")
-            // return new Error("Product is not found", product)
         }
-        console.log("ROPD", product)
         res.render("admin/edit-product", {
             title:"Editing My Product",
             path:"/admin/edit-product",
@@ -29,16 +26,26 @@ module.exports.getEditProduct = (req, res, next) => {
             product: product
         });
     })
-
-
 };
+
+module.exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDesc = req.body.description
+    const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice)
+    updatedProduct.save();
+    res.redirect('/admin/products')
+
+}
 
 module.exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.redirect('/')
 };
--- expressServer/models/product.js
+++ expressServer/models/product.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-const p = path.join(__dirname, "..",  'data', 'products.json');
+const p = path.join(__dirname, "..", 'data', 'products.json');
 
-const getProductsFromFile = (callback) =>{
+const getProductsFromFile = (callback) => {
     fs.readFile(p, (err, fileContent) => {
         if (err) {
             callback([])
@@ -12,27 +12,39 @@ const getProductsFromFile = (callback) =>{
 };
 
 module.exports = class Product {
-    constructor(title, imageUrl, description, price){
+    constructor(id, title, imageUrl, description, price) {
+        this.id = id;
         this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
         this.price = price
     }
 
-    save(){
-        this.id = Math.random().toString()
+    save() {
         getProductsFromFile(products => {
-            products.push(this)
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log("ERR", err)
-            } )
+            if (this.id){
+                const existingProductIndex= products.findIndex(product => product.id === this.id)
+                const updatedProducts = [...products]
+                updatedProducts[existingProductIndex] = this
+                fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+                    console.log("ERR", err)
+                })
+            }else{
+                this.id = Math.random().toString()
+                products.push(this)
+                fs.writeFile(p, JSON.stringify(products), (err) => {
+                    console.log("ERR", err)
+                })
+            }
         })
     }
+
 //since the func does not return anything, we put call back to get value from fs.readFile once it is ready
     static fetchAll(callback) {
         getProductsFromFile(callback)
     }
-    static findById(id, cb){
+
+    static findById(id, cb) {
         getProductsFromFile(products => {
             const product = products.find(p => p.id === id);
             cb(product)
