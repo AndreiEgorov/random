61be97d13df03b2c5b7f8906795b5d91282f6d06
Andrei Egorov <andreyegorov1992@gmail.com>
2019-07-08 08:32:29 -0700

    REST api, support POST requests

--- restApi/app.js
+++ restApi/app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json())  //parses data application/json
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Access-Control-Allow-METHODS", "GET, POST, PUT")
-    res.setHeader("Access-Control-Allow-METHODS", "Content-type, Authorization")
+    res.setHeader("Access-Control-Allow-Headers", "Content-type, Authorization")
     next()
 })
 app.use("/feed", feedRouter);
@@ -19,14 +19,30 @@ app.listen(8080)
 //to test CORS request add to codepen to send a request
 // <button id="get">Get posts</button>
 
-
+//
 // const getButton = document.getElementById('get')
-
-
+// const postButton = document.getElementById('post')
+//
 // getButton.addEventListener('click', () => {
 //     fetch('http://localhost:8080/feed/posts')
 //         .then(res => res.json() )
 //         .then(resData => console.log("BOB", resData))
 //         .catch(err => console.log("ROSO", err))
 // })
-
+//
+// postButton.addEventListener('click', () => {
+//     fetch('http://localhost:8080/feed/post', {
+//         method: "POST",
+//         body: JSON.stringify({
+//             title: "A codepen post",
+//             content: "Codepen content"
+//         }),
+//         headers:{
+//             'Content-Type': 'application/json'
+//         }
+//
+//     })
+//         .then(res => res.json())
+//         .then(resData => console.log("BOB", resData))
+//         .catch(err => console.log("ROSO", err))
+// })
